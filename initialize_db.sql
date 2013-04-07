e02c24e10880180b4c11fc8811e1d01f39f401f2
rishi1986 <hdeshmukh1611@gmail.com>
2013-04-06 17:53:17 -0700

    Update initialize_db.sql

--- initialize_db.sql
+++ initialize_db.sql
@@ -1,12 +1,37 @@
-/* Query for deleting a locations table */
-drop table if exists locations;
 
-/* Query for creating a locations table */
-create table locations(
-	id integer primary key autoincrement,
-	user_id varchar,
-	lat float,
-	long float,
-	address varchar,
-	name varchar);
+
+
+create table game(
+	id integer primary key,
+	player1_id text,
+	player2_id text,
+	img_url text,
+	label text unique key,
+	player1_misses integer,
+	player2_misses integer,
+	winner text,
+	FOREIGN KEY(player1_id) references players(fb_id),
+	FOREIGN KEY(player2_id) references players(fb_id),
+	FOREIGN KEY(winner) references players(fb_id)
+);
+
+
+create table moves(
+  	id integer primary key
+  	game_id integer 
+        player_id text
+  	type text default 'upload'
+	img_url text
+	label text unique key
+	result text default 'N'
+	FOREIGN KEY(game_id) references game(id)
+	FOREIGN KEY(player_id) references players(id)
+);
+	
+
+create table players(
+	fb_id text
+	games_played integer  default 0
+	games_won integer default 0
+);
 
